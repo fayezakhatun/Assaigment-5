606f1c8bfde13bb603e14c00836734fc6f6f0019
fayeza khatun <fayezaaktarjoya@gmail.com>
2026-05-03 23:24:30 +0600

    all section commited

--- login.js
+++ login.js
@@ -2,7 +2,7 @@ document.getElementById('login-btn').addEventListener('click',function(){
     
     const usernameInput=document.getElementById('input-username').value;
     //2-get the pasword 
-    const paswordInput=document.getElementById('input-password').value;
+    const passwordInput=document.getElementById('input-password').value;
     console.log(passwordInput)
     //3-match pin & username
     if(usernameInput=="admin" && passwordInput=="admin123"){
